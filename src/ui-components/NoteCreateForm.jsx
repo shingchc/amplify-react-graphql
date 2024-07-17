22862a41511d8746e075375b69d7e352eb9d0a00
shingchc <scchan610@gmail.com>
2024-07-17 16:01:50 -0700

    Testing

--- src/ui-components/NoteCreateForm.jsx
+++ src/ui-components/NoteCreateForm.jsx
@@ -127,7 +127,7 @@ export default function NoteCreateForm(props) {
       {...rest}
     >
       <TextField
-        label="Name"
+        label="Namess"
         isRequired={true}
         isReadOnly={false}
         value={name}
